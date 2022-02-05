5dd51519066f56dccfb7920faa59bdcf1fc696f4
Krestyaninov12 <kreda@pnpe.ru>
2022-02-06 01:36:51 +0300

    Добавьте файлы проекта.

---
+++ Lab 2.19/ViewModels/CallerMemberNameAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_41_WPF_19.ViewModels
+{
+    internal class CallerMemberNameAttribute : Attribute
+    {
+    }
+}
